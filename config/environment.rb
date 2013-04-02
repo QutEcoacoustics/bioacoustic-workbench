ee76af65914ffb2f38cde0a09c7dbc7bd52ddc3b
Anthony Truskinger <qut.bioacoustics.research+anthony@gmail.com>
2013-04-02 14:26:07 +1000

    trying dummy certificate

--- config/environment.rb
+++ config/environment.rb
@@ -1,5 +1,8 @@
 # Load the rails application
 require File.expand_path('../application', __FILE__)
 
+# Load certificate bundler
+OpenID.fetcher.ca_file = "#{Rails.root}/config/ca-bundle.crt"
+
 # Initialize the rails application
 BawSite::Application.initialize!
